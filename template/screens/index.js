65cd778fe335bb213f12e87ddf44272319350268
fgreil <f.greil@tum.de>
2025-12-07 22:44:36 +0100

    Create index.js

---
+++ template/screens/index.js
@@ -0,0 +1,6 @@
+export { default as ExploreScreen } from "./ExploreScreen";
+export { default as FavoritesScreen } from "./FavoritesScreen";
+export { default as HistoryScreen } from "./HistoryScreen";
+export { default as ArticleScreen } from "./ArticleScreen";
+export { default as SearchScreen } from "./SearchScreen";
+export { default as SearchFilterScreen } from "./SearchFilterScreen";
